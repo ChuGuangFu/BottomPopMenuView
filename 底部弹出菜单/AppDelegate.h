6c4c23bbf5fcefbb5f5338a21f3ba0797fdc1c26
初光夫 <chuguangfu@chuguangfudeiMac.local>
2018-12-17 18:04:57 +0800

    Change

--- 底部弹出菜单/AppDelegate.h
+++ 底部弹出菜单/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//  底部弹出菜单
-//
-//  Created by 处光夫 on 2018/12/17.
-//  Copyright © 2018 处光夫. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
@@ -12,6 +5,4 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
